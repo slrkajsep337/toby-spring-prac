3cd3bd58938198eab921537a39dcd2fac16600c1
이수진 <slrkajsep337@naver.com>
2022-10-25 11:39:02 +0900

    create user, userDao

---
+++ src/main/java/com/line/dao/UserDao.java
@@ -0,0 +1,59 @@
+package com.line.dao;
+
+import com.line.domain.User;
+
+import java.sql.*;
+import java.util.Map;
+
+public class UserDao {
+    public void add(User user) {
+        Map<String, String> env = System.getenv();
+        try {
+            Connection conn = DriverManager.getConnection(env.get("DB_HOST"),
+                    env.get("DB_NAME"), env.get("DB_PASSWORD"));
+            PreparedStatement ps = conn.prepareStatement(
+                    "INSERT INTO USERS(id, name, password) VALUES(?,?,?);");
+            ps.setString(1, user.getId());
+            ps.setString(2, user.getName());
+            ps.setString(3, user.getPassword());
+
+            ps.executeUpdate();
+            ps.close();
+            conn.close();
+        } catch(SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public User findById(String id) {
+        Map<String, String> env = System.getenv();
+        Connection conn;
+        try {
+            conn = DriverManager.getConnection(env.get("DB_HOST"),
+                    env.get("DB_NAME"), env.get("DB_PASSWORD"));
+            PreparedStatement ps = conn.prepareStatement(
+                    "select * from users where id=?"
+            );
+            ps.setString(1,id);
+
+            ResultSet rs = ps.executeQuery();
+            rs.next();
+            User user = new User(rs.getString("id"),
+                    rs.getString("name"), rs.getString("password"));
+            rs.close();
+            ps.close();
+            conn.close();
+
+            return user;
+
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public static void main(String[] args) {
+        UserDao ud = new UserDao();
+        User user = ud.findById("1");
+        System.out.println(user.getName());
+    }
+}
