8d83e2cf5978466dbd090185f22851ac0e3feb1c
이수진 <slrkajsep337@naver.com>
2022-10-25 13:27:39 +0900

    test update

--- src/test/java/com/line/dao/UserDaoTest.java
+++ src/test/java/com/line/dao/UserDaoTest.java
@@ -10,17 +10,24 @@ public class UserDaoTest {
     @Test
     void addAndSelect() {
         //분리전 test
-        UserDao ud = new UserDao();
-        String id = "1";
-        ud.add(new User(id, "Rara", "11447788"));
-        User user = ud.findById(id);
-        Assertions.assertEquals("Rara", user.getName());
+//        UserDao ud = new UserDao();
+//        String id = "1";
+//        ud.add(new User(id, "Rara", "11447788"));
+//        User user = ud.findById(id);
+//        Assertions.assertEquals("Rara", user.getName());
+//
+//        //분리후 test
+//        UserDao ud2 = new UserDao();
+//        String id2 = "2";
+//        ud.add(new User(id, "Raraa", "114477889"));
+//        user = ud.findById(id);
+//        Assertions.assertEquals("Raraa", user.getName());
 
-        //분리후 test
-        UserDao ud2 = new UserDao();
-        String id2 = "2";
-        ud.add(new User(id, "Raraa", "114477889"));
-        user = ud.findById(id);
-        Assertions.assertEquals("Raraa", user.getName());
+        //interface적용 후
+        UserDao ud = new UserDao(new ConnectionA());
+        String id = "3";
+        ud.add(new User(id, "Raro", "114788"));
+        User user = ud.findById(id);
+        Assertions.assertEquals("Raro", user.getName());
     }
 }
